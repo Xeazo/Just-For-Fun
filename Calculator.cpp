40cf4d10fa0871b67e90983ed4273dcbd39faafa
Michael Aslan <89019254+Xeazo@users.noreply.github.com>
2021-09-26 17:12:22 +0700

    Update Calculator.cpp

--- Calculator.cpp
+++ Calculator.cpp
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
 
 int main()
 {
-	puts ("Pilih Operasi Yang Akan Anda Lakukan (1-2-3-4)");
+	puts ("Pilih Operasi Yang Akan Anda Lakukan (1-2-3-4-5)");
 	puts ("1 = Penjumlahan");
 	puts ("2 = Pengurangan");
 	puts ("3 = Perkalian");
 	puts ("4 = Pembagian");
+	puts ("5 = Pangkat");
 	
 	int n;
 	int x,y,o;	
@@ -18,20 +20,39 @@ int main()
 	switch (n)
 	{
 		case 1:
-			scanf ("%d %d", &x, &y);
+			printf("Angka Pertama :");
+			scanf ("%d", &x);
+			printf("Angka Kedua :");
+			scanf ("%d", &y);
 			o = x+y;
 			break;
 		case 2:
-			scanf ("%d %d", &x, &y);
+			printf("Angka Pertama :");
+			scanf ("%d", &x);
+			printf("Angka Kedua :");
+			scanf ("%d", &y);
 			o = x-y;
 			break;
 		case 3:
-			scanf ("%d %d", &x, &y);
+			printf("Angka Pertama :");
+			scanf ("%d", &x);
+			printf("Angka Kedua :");
+			scanf ("%d", &y);
 			o = x*y;
 			break;
 		case 4:
-			scanf ("%d %d", &x, &y);
+			printf("Angka Pertama :");
+			scanf ("%d", &x);
+			printf("Angka Kedua :");
+			scanf ("%d", &y);
 			o = x/y;
+			break;
+		case 5:
+			printf("Angka Pertama :");
+			scanf ("%d", &x);
+			printf("Pangkat :");
+			scanf ("%d", &y);
+			o = pow(x,y);
 			break;
 		
 	}
